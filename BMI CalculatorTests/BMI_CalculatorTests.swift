d9ed1b21af388635f7f1e9134fa8a0b8db0d1b04
razeiv07 <rajeevxrma@gmail.com>
2024-10-20 13:47:30 -0400

    Initial Commit

---
+++ BMI CalculatorTests/BMI_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorTests.swift
+//  BMI CalculatorTests
+//
+//  Created by Rajeev Sharma on 2024-10-20.
+//
+
+import Testing
+@testable import BMI_Calculator
+
+struct BMI_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
