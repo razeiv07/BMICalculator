5d7fe12d2c5b836d9fa0caad1187cb37986aa44e
razeiv07 <rajeevxrma@gmail.com>
2024-10-21 22:42:44 -0400

    Calculate button works as well as the value changes in the height and weight label when the slider is moved

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,18 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(String(format: "%.2f", sender.value))
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height) m"
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight) kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
         
-        print(Int(sender.value))
+        let bmi = weight / pow(height, 2)
+        print(bmi)
     }
 }
 
